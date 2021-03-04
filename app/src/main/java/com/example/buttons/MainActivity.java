9c51a6e724ac623bcdd54f9acf77348441518ee8
ADNAN <adnanmustafa7>
2021-03-04 16:10:23 +0500

    Buttons

---
+++ app/src/main/java/com/example/buttons/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.buttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+        protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttons);
+
+    }
+}
